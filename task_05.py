84bb09b9be4cf9ca62936edef9f54806e4f2155f
Tollevin Williams <tollevin.williams@spsmail.cuny.edu>
2014-09-22 06:41:24 -0400

    Ok

--- task_05.py
+++ task_05.py
@@ -7,9 +7,9 @@ import data
 MATCHUPS = ''
 COUNTER = 0
 
-for ROW_INDEX, ROW_NAME in enumerate(data.VERSUS):
-    for COLUMN_INDEX, COLUMN_NAME in enumerate(data.VERSUS):
-        if ROW_INDEX < COLUMN_INDEX:
+for COLUMN_INDEX, COLUMN_NAME in enumerate(data.VERSUS):
+    for ROW_INDEX, ROW_NAME in enumerate(data.VERSUS):
+        if COLUMN_INDEX > ROW_INDEX:
             COUNTER += 1
             MATCHUPS += ('{0},"{1}","{2}"'.format(COUNTER, COLUMN_NAME, ROW_NAME) + "\n")
 
